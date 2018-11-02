94908e1771d9395091ba236038f95154e3fd1585
Sander <sander.vispoel@labela.nl>
2018-11-02 11:16:03 +0100

    Better logs & overwrite project package defaults

--- index.js
+++ index.js
@@ -24,4 +24,26 @@ var commands = [
 
 cmd.run(commands);
 
-console.log('Installing...');
+console.log('Cloning React-Prime...');
+
+// Wait for project folder to exist
+while(!fs.existsSync(`./${projectName}`)) {}
+
+// Wait for node_modules folder to exist to check if we are in the "npm i" phase
+while(!fs.existsSync(`./${projectName}/node_modules`)) {}
+
+console.log('Installing packages...');
+
+var projectPkgPath = `${projectName}/package.json`;
+var pkgRead = fs.readFileSync(projectPkgPath, 'utf8');
+var pkgParsed = JSON.parse(pkgRead);
+
+// Overwrite react-prime defaults
+pkgParsed.name = projectName;
+pkgParsed.version = '0.0.1';
+pkgParsed.description = projectName;
+pkgParsed.author = 'Label A [labela.nl]';
+pkgParsed.repository.url = '';
+pkgParsed.keywords = [];
+
+fs.writeFileSync(projectPkgPath, JSON.stringify(pkgParsed, null, 2));
