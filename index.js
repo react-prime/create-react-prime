ab37c858fb81f1a2d170ecfa63fb5dc458ca01a0
Sander <sander.vispoel@labela.nl>
2018-11-02 10:26:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var program = require('commander');
+var cmd = require('node-cmd');
+var fs = require('fs');
+var pkg = require('./package.json');
+
+program
+  .version(pkg.version)
+  .parse(process.argv);
+
+var projectName = program.args[0] || 'react-prime';
+
+if (fs.existsSync(projectName)) {
+  return console.error(`Error: directory '${projectName}' already exists.`);
+}
+
+cmd.run(`git clone https://github.com/JBostelaar/react-prime.git ${projectName} && rm -rf ${projectName}/.git`);
