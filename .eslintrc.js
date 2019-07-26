394068fe5a85269a300293a01fc5107224415611
Sander Vispoel <sander.vispoel@labela.nl>
2019-07-26 15:28:57 +0200

    Add editor config & add max line length rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,25 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true,
-        'node': true,
-        'mocha': true,
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        mocha: true,
     },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+    extends: 'eslint:recommended',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
     },
-    'parserOptions': {
-        'ecmaVersion': 2018
+    parserOptions: {
+        ecmaVersion: 2018
     },
-    'rules': {
-        'indent': [
-            'error',
-            2,
-            {
-                'SwitchCase': 1
-            }
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
+    rules: {
+        indent: ['error', 2, { SwitchCase: 1 }],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'max-len': ['error', { code: 100 }],
+        'linebreak-style': ['error', 'unix'],
         'no-trailing-spaces': 1,
         'no-multiple-empty-lines': 1,
         'no-console': ["error", { "allow": ["warn", "error"] }],
