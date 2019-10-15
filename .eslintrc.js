40ce4700ba939cf726c2d14e66ade3bbca9108f4
Jorn Bostelaar <bostelaarj@gmail.com>
2019-10-15 10:59:17 +0200

    Fix indenting in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        node: true,
-        mocha: true,
-    },
-    extends: 'eslint:recommended',
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-        ecmaVersion: 2018
-    },
-    rules: {
-        indent: ['error', 2, { SwitchCase: 1 }],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-        'max-len': ['error', { code: 100 }],
-        'linebreak-style': ['error', 'unix'],
-        'no-trailing-spaces': 1,
-        'no-multiple-empty-lines': 1,
-        'no-console': ["error", { "allow": ["warn", "error"] }],
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'max-len': ['error', { code: 100 }],
+    'linebreak-style': ['error', 'unix'],
+    'no-trailing-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'no-console': ["error", { "allow": ["warn", "error"] }],
+  }
 };
