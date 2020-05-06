47f53938c9a6d341ef6d2b72574a91cba8b6e2b4
Sander Vispoel <sander.vispoel@labela.nl>
2020-05-06 18:34:21 +0200

    Install and configure Typescript and other dependencies

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+const tsConfigPaths = require('tsconfig-paths');
+const tsConfig = require('./tsconfig.json');
+
+const baseUrl = './dist';
+tsConfigPaths.register({
+  baseUrl,
+  paths: tsConfig.compilerOptions.paths,
+});
