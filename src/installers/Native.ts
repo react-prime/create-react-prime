23a8e36674af0abc4bf5a78c38748ad7b2496a46
Sander Vispoel <sander.vispoel@labela.nl>
2020-05-07 22:03:16 +0200

    Add updatePackage override for Native

--- src/installers/Native.ts
+++ src/installers/Native.ts
@@ -6,7 +6,19 @@ export default class NativeInstaller extends Installer {
     super();
 
     App.setProjectName(
-      App.getProjectName().replace(/\W/g, ''),
+      App.getInstallConfig().projectName.replace(/\W/g, ''),
     );
+  }
+
+  protected async updatePackage() {
+    const { projectName } = App.getInstallConfig();
+    const pkg = App.getProjectNpmPackage().json;
+
+    pkg.scripts = pkg.scripts || {};
+    pkg.scripts.renameNative = `npx react-native-rename ${projectName}`;
+    pkg.scripts.replaceWithinFiles = `npx replace 'reactprimenative' '${projectName}' . -r --exclude="package*.json"`;
+    pkg.scripts.replaceSchemeFilenames = `npx renamer -d --find "/reactprimenative/g" --replace "${projectName}" "**"`;
+
+    super.updatePackage(pkg);
   }
 }
