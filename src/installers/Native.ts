33a702bab66e00551afcf583adc1bd6a356931a5
Sander Vispoel <sander.vispoel@labela.nl>
2020-05-13 18:17:01 +0200

    Add script key strings as property

--- src/installers/Native.ts
+++ src/installers/Native.ts
@@ -2,11 +2,23 @@ import InstallConfig from '../InstallConfig';
 import Installer from './Installer';
 
 export default class NativeInstaller extends Installer {
+  private scriptKeys: {
+    rename: string;
+    replaceText: string;
+    replaceSchemes: string;
+  };
+
   constructor() {
     // Native project names can only contain alphanumerical characters
     InstallConfig.projectName = InstallConfig.projectName.replace(/\W/g, '');
 
     super();
+
+    this.scriptKeys = {
+      rename: 'renameNative',
+      replaceText: 'replaceWithinFiles',
+      replaceSchemes: 'replaceSchemeFilenames',
+    };
 
     // Run additional scripts after NPM install
     this.addInstallStep({
@@ -23,9 +35,11 @@ export default class NativeInstaller extends Installer {
     const pkg = this.getProjectNpmPackage().json;
 
     pkg.scripts = pkg.scripts || {};
-    pkg.scripts.renameNative = `npx react-native-rename ${projectName}`;
-    pkg.scripts.replaceWithinFiles = `npx replace 'reactprimenative' '${projectName}' . -r --exclude="package*.json"`;
-    pkg.scripts.replaceSchemeFilenames = `npx renamer -d --find "/reactprimenative/g" --replace "${projectName}" "**"`;
+    pkg.scripts[this.scriptKeys.rename] = `npx react-native-rename ${projectName}`;
+    pkg.scripts[this.scriptKeys.replaceText] =
+      `npx replace 'reactprimenative' '${projectName}' . -r --exclude="package*.json"`;
+    pkg.scripts[this.scriptKeys.replaceSchemes] =
+      `npx renamer -d --find "/reactprimenative/g" --replace "${projectName}" "**"`;
 
     super.updatePackage(pkg);
   }
@@ -33,9 +47,9 @@ export default class NativeInstaller extends Installer {
   protected async cleanup(): Promise<void> {
     const pkg = this.getProjectNpmPackage().json;
 
-    delete pkg.scripts.renameNative;
-    delete pkg.scripts.replaceWithinFiles;
-    delete pkg.scripts.replaceSchemeFilenames;
+    delete pkg.scripts[this.scriptKeys.rename];
+    delete pkg.scripts[this.scriptKeys.replaceText];
+    delete pkg.scripts[this.scriptKeys.replaceSchemes];
 
     await this.writeToPackage(pkg);
     await super.cleanup();
@@ -44,8 +58,8 @@ export default class NativeInstaller extends Installer {
 
   // Run the additional scripts
   private async runScripts(): Promise<void> {
-    await this.asyncSpawn('npm', ['run', 'renameNative']);
-    await this.asyncSpawn('npm', ['run', 'replaceWithinFiles']);
-    await this.asyncSpawn('npm', ['run', 'replaceSchemeFilenames']);
+    await this.asyncSpawn('npm', ['run', this.scriptKeys.rename]);
+    await this.asyncSpawn('npm', ['run', this.scriptKeys.replaceText]);
+    await this.asyncSpawn('npm', ['run', this.scriptKeys.replaceSchemes]);
   }
 }
