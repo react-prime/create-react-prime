e598a8f10d2ab8441e2b19abe95c68956b6f19e3
Sander Vispoel <sander.vispoel@labela.nl>
2020-06-17 13:42:10 +0200

    Remove unnecessary packages & replace dev script
    Use `npm run dev --` to add options, I.e. `npm run dev -- -t native`

--- tsconfig-paths-bootstrap.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const tsConfigPaths = require('tsconfig-paths');
-const tsConfig = require('./tsconfig.json');
-
-const baseUrl = './dist';
-tsConfigPaths.register({
-  baseUrl,
-  paths: tsConfig.compilerOptions.paths,
-});
