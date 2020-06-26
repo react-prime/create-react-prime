9a6bc5004e806dfdc6fa53e8dbdbedf95b3b2547
Sander Vispoel <sander.vispoel@labela.nl>
2020-06-26 14:48:57 +0200

    Show version before install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
+const fs = require('fs');
+const path = require('path');
 const { TEXT } = require('./constants');
 const { name } = require('./installConfig');
 const install = require('./install');
 
+const pkgRead = fs.readFileSync(path.resolve('package.json'), 'utf8');
+const pkg = JSON.parse(pkgRead);
+
 /*
   Start install process
 */
+
+console.log(`create-react-prime v${pkg.version}`);
+
 install()
   .then(() => {
     console.log(`⚡️ ${TEXT.BOLD} Succesfully installed ${name}! ${TEXT.DEFAULT}`);
