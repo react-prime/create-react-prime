7ec0685fb767f65a54d4dbe4210bd26780cdf7f7
Sander Vispoel <sander.vispoel@labela.nl>
2020-06-26 14:46:40 +0200

    Remove polyfill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-require('./polyfill');
 const { TEXT } = require('./constants');
 const { name } = require('./installConfig');
 const install = require('./install');
@@ -10,7 +9,7 @@ const install = require('./install');
   Start install process
 */
 install()
-  .finally(() => {
+  .then(() => {
     console.log(`⚡️ ${TEXT.BOLD} Succesfully installed ${name}! ${TEXT.DEFAULT}`);
 
     process.exit();
