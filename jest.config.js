da2be0eb8a67d8cd8c4c0c5509ca07f944ec8b75
Sander Vispoel <sander.vispoel@labela.nl>
2020-06-27 16:18:40 +0200

    Add Jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'dist',
+  ],
+};
