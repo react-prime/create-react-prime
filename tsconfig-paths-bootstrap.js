0bb3113c1368f303d2e1498d961f3fcf8bae5f36
sandervspl <sandervispoel@hotmail.com>
2021-02-03 16:00:21 +0100

    Attempt to fix npx

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -1,7 +1,8 @@
+const path =require('path');
 const tsConfigPaths = require('tsconfig-paths');
 const tsConfig = require('./tsconfig.json');
 
 tsConfigPaths.register({
-  baseUrl: './dist/',
+  baseUrl: path.resolve('dist'),
   paths: tsConfig.compilerOptions.paths,
 });
