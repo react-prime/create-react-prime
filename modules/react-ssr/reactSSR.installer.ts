ba959277570736c49cca9f39b07f70e41f0f1f2f
sandervspl <sandervispoel@hotmail.com>
2021-04-06 18:52:09 +0200

    Delete reactSSR.installer.ts

--- modules/react-ssr/reactSSR.installer.ts
+++
@@ -1,8 +0,0 @@
-import Installer from 'core/decorators/Installer';
-
-
-@Installer({
-  name: 'react-ssr',
-  cloneUrl: 'https://github.com/react-prime/react-prime-ssr.git',
-})
-export default class ReactSSRInstaller {}
