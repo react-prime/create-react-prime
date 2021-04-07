3b9267f4991cedbc3f253fd85f5121df95dc5b6e
sandervspl <sandervispoel@hotmail.com>
2021-04-07 15:16:50 +0200

    Revert cli bootstrap changes

--- core/CLIMgr.ts
+++ core/CLIMgr.ts
@@ -1,32 +1,13 @@
 import bootstrapCLI from './cli';
 
-import { ARG, ERROR_TEXT } from 'core/constants';
-import Validate from 'core/Validate';
-import Logger from 'core/Logger';
+import { ARG } from 'core/constants';
 
 
 // Create CLI ASAP in runtime
-const cliAPI = (() => {
-  const cli = bootstrapCLI();
-
-  // Parse user input
-  cli.parse(process.argv);
-
-  // Validate project name
-  if (cli.args[ARG.ProjectName] != null) {
-    const validate = new Validate();
-    const logger = new Logger();
-
-    if (!validate.folderName(cli.args[ARG.ProjectName])) {
-      logger.error(ERROR_TEXT.ProjectName);
-    }
-  }
-
-  return cli;
-})();
+export const cliAPI__DO_NOT_USE__ = bootstrapCLI();
 
 class CLIMgr {
-  private cli = cliAPI;
+  private cli = cliAPI__DO_NOT_USE__;
   private projectName: string = this.cli.args[ARG.ProjectName];
   private boilerplate: string = this.cli.opts().boilerplate;
 
--- core/cli.ts
+++ core/cli.ts
@@ -1,15 +1,32 @@
 import commander, { Command } from 'commander';
 
+import { ARG, ERROR_TEXT } from 'core/constants';
+import Validate from 'core/Validate';
+import Logger from 'core/Logger';
+
 
 export default function bootstrapCLI(): CLI {
   const cli = new Command() as CLI;
+
+  cli.version(process.env.VERSION!);
 
   cli.option(
     '-b, --boilerplate <boilerplate>',
     'Install chosen boilerplate. Options: react-spa, react-ssr, react-native',
   );
 
-  cli.version(process.env.VERSION!);
+  // Parse user input
+  cli.parse(process.argv);
+
+  // Validate project name
+  if (cli.args[ARG.ProjectName] != null) {
+    const validate = new Validate();
+    const logger = new Logger();
+
+    if (!validate.folderName(cli.args[ARG.ProjectName])) {
+      logger.error(ERROR_TEXT.ProjectName);
+    }
+  }
 
   return cli;
 }
