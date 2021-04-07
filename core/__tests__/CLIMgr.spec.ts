8a77b0fdbb91604a8f6b9d541ad4bc001c1127f9
sandervspl <sandervispoel@hotmail.com>
2021-04-07 15:16:55 +0200

    Add CLIMgr tests

---
+++ core/__tests__/CLIMgr.spec.ts
@@ -0,0 +1,39 @@
+import cliMgr, { cliAPI__DO_NOT_USE__ } from 'core/CLIMgr';
+
+
+describe('CLIMgr', () => {
+  function parse(...str: string[]) {
+    cliAPI__DO_NOT_USE__.parse(['node', 'dist/main.js', ...str]);
+  }
+
+  it('Returns the correct project name', () => {
+    const name = 'projectname';
+    parse(name);
+
+    expect(cliMgr.getProjectName()).toBe(name);
+  });
+
+  it('Returns the correct boilerplate', () => {
+    const bp = 'react-spa';
+
+    parse('-b', bp);
+    expect(cliMgr.getBoilerplate()).toBe(bp);
+
+    parse('--boilerplate', bp);
+    expect(cliMgr.getBoilerplate()).toBe(bp);
+  });
+
+  it('Sets the project name correctly', () => {
+    const name = 'projectname';
+    cliMgr.setProjectName(name);
+
+    expect(cliMgr.getProjectName()).toBe(name);
+  });
+
+  it('Sets the boilerplate correctly', () => {
+    const bp = 'react-spa';
+    cliMgr.setBoilerplate(bp);
+
+    expect(cliMgr.getBoilerplate()).toBe(bp);
+  });
+});
