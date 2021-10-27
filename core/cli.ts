369164e8bb8684162d0a2fe029c69b5289b720ba
sandervspl <sandervispoel@hotmail.com>
2021-10-27 17:07:59 +0200

    Dynamically get installable boilerplate names

--- core/cli.ts
+++ core/cli.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import commander, { Command } from 'commander';
 
 import { ARG, ERROR_TEXT } from 'core/constants';
@@ -10,9 +12,15 @@ export default function bootstrapCLI(): commander.Command {
 
   cli.version(process.env.VERSION!);
 
+  // Extract the names of all installable boilerplates from their folder name
+  const modules = fs.readdirSync(path.resolve('modules'), { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
+    .filter((name) => name.toLowerCase() !== 'defaults');
+
   cli.option(
     '-b, --boilerplate <boilerplate>',
-    'Install chosen boilerplate. Options: react-spa, react-ssr, react-native',
+    `Install chosen boilerplate. Options: ${modules.join(', ')}`,
   );
 
   // Parse user input
