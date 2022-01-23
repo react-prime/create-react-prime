a3fa42846bb21e940fff375b0fa9bf445e1d22e1
sandervspl <sandervispoel@hotmail.com>
2022-01-23 17:35:10 +0100

    Add questions

--- modules_v3/questions.ts
+++ modules_v3/questions.ts
@@ -1,4 +1,4 @@
-import { prompt, DistinctQuestion } from 'inquirer';
+import { prompt, DistinctQuestion, Answers } from 'inquirer';
 
 
 const question = async <Q extends DistinctQuestion>(obj: Q): Promise<string> => {
@@ -9,17 +9,59 @@ const question = async <Q extends DistinctQuestion>(obj: Q): Promise<string> => 
 };
 
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-export const boilerplateQuestion = async () => question({
+export const boilerplate = () => question({
   type: 'list',
   name: 'What boilerplate would you like to install?',
   choices: ['react-web', 'react-mobile'],
   default: 0,
 });
 
-export const renderingQuestion = async () => question({
+export const rendering = () => question({
   type: 'list',
   name: 'How should the app be rendered?',
   choices: ['client-side', 'server-side + static'],
   default: 0,
 });
+
+export const projectName = (answers: Answers) => question({
+  type: 'input',
+  name: 'Project name',
+  default: answers.boilerplate,
+});
+
+export const cms = () => question({
+  type: 'list',
+  name: 'What CMS will the project use?',
+  choices: [{
+    name: 'Skip',
+    value: null,
+  }, {
+    name: 'Contentful',
+    value: 'contentful',
+  }],
+  default: 0,
+});
+
+export const modules = () => question({
+  type: 'list',
+  name: 'What CMS will the project use?',
+  choices: [{
+    name: 'Skip',
+    value: null,
+  }, {
+    name: 'Contentful',
+    value: 'contentful',
+  }],
+  default: 0,
+});
+
+export const openInEditor = () => question({
+  type: 'list',
+  name: 'Open project in editor?',
+  choices: [{
+    name: 'Skip',
+    value: null,
+  }],
+});
+
 /* eslint-enable */
