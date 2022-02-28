c6ad01142ca8b10cd2c1948e70ba53c038c056a3
Sander Vispoel <sandervispoel@hotmail.com>
2022-03-01 00:19:52 +0100

    Create constants.spec.ts

---
+++ __tests__/constants.spec.ts
@@ -0,0 +1,11 @@
+import os from 'os';
+import { spyOn } from 'vitest';
+
+describe('constants', () => {
+  it('Returns a valid .crp.json path', async () => {
+    spyOn(os, 'homedir').mockImplementationOnce(() => '/foo/bar');
+    const { SETTINGS_FILE_PATH } = await import('@crp/constants');
+
+    expect(SETTINGS_FILE_PATH).toBe('/foo/bar/.crp.json');
+  });
+});
