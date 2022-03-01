d578649f6331854dc388a06228639d0f5dcd52db
Sander Vispoel <sandervispoel@hotmail.com>
2022-03-01 10:55:25 +0100

    Fix cli flags working incorrectly

--- cli/options.ts
+++ cli/options.ts
@@ -3,12 +3,8 @@ import type { Command } from 'commander';
 
 function addOptions(cli: Command): void {
   cli
-    .option('-b, --boilerplate')
-    .description('Install a boilerplate');
-
-  cli
-    .option('-d, --debug')
-    .description('Enable debug mode (verbose logging)');
+    .option('-b, --boilerplate', 'Install a boilerplate')
+    .option('-d, --debug', 'Enable debug mode (verbose logging)');
 }
 
 export default addOptions;
