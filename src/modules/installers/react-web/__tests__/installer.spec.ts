7d12beaf17cca7f9a70f754691753794b6a7626d
Sander Vispoel <sandervispoel@hotmail.com>
2022-03-02 13:36:59 +0100

    Add react-web installer tests

--- src/modules/installers/react-web/__tests__/installer.spec.ts
+++ src/modules/installers/react-web/__tests__/installer.spec.ts
@@ -10,13 +10,6 @@ vi.mock('../../shared/actions', () => ({
   cleanup: vi.fn(),
 }));
 
-// Mock module actions
-vi.mock('../actions', () => ({
-  validateProjectName: vi.fn(),
-  renameProject: vi.fn(),
-  renameFiles: vi.fn(),
-}));
-
 // Mock logger
 vi.mock('@crp/utils', () => ({
   logger: {
@@ -24,8 +17,17 @@ vi.mock('@crp/utils', () => ({
   },
 }));
 
+// Mock questions
+vi.mock('../../../questions', () => ({
+  rendering: vi.fn(),
+  cms: vi.fn(),
+  modules: vi.fn(),
+  openInEditor: vi.fn(),
+  answerOpenInEditor: vi.fn(),
+}));
 
-describe('react-mobile installer', () => {
+
+describe('react-web installer', () => {
   it('Opens the editor if user chooses an editor', async () => {
     vi.spyOn(question, 'openInEditor').mockResolvedValueOnce({ name: 'foo', search: 'bar' });
     const answerSpy = vi.spyOn(question, 'answerOpenInEditor')
