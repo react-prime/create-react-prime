dbd5259e24e4920d5c181c06e6ea1950f54429b8
Sander Vispoel <sandervispoel@hotmail.com>
2022-03-02 11:28:02 +0100

    Create entry.spec.ts

---
+++ src/cli/__tests__/entry.spec.ts
@@ -0,0 +1,38 @@
+import { spyOn } from 'vitest';
+
+import { getActionForOption } from '../actions/entry';
+import * as question from '../../modules/questions';
+
+
+describe('Entry', () => {
+  it('Finds the entry point of a given option', async () => {
+    const action = await getActionForOption({ boilerplate: true });
+    expect(action).toBeDefined();
+  });
+
+  it('Prompts for entry point if entry point is not found', async () => {
+    const promptSpy = spyOn(question, 'entry').mockResolvedValue('boilerplate');
+
+    let action = await getActionForOption({});
+
+    expect(action).toBeDefined();
+    expect(promptSpy).toHaveBeenCalledTimes(1);
+
+    // @ts-expect-error
+    action = await getActionForOption({ foo: true });
+
+    expect(action).toBeDefined();
+    expect(promptSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('Exits if user chooses to exit', async () => {
+    const exitSpy = spyOn(process, 'exit').mockImplementationOnce(() => void 0 as never);
+    const promptSpy = spyOn(question, 'entry').mockImplementationOnce(() => Promise.resolve(null));
+
+    const action = await getActionForOption({});
+
+    expect(action).toBeDefined();
+    expect(promptSpy).toHaveBeenCalled();
+    expect(exitSpy).toHaveBeenCalled();
+  });
+});
