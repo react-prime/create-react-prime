4d6bf9294feb186706debc1dcf5d4c678e487174
Ronny Rook <ronny@labela.nl>
2022-04-07 10:05:25 +0200

    Use cwd instead of cd for sourcing iOS folder for Podfile

--- src/modules/installers/react-mobile/actions.ts
+++ src/modules/installers/react-mobile/actions.ts
@@ -20,7 +20,9 @@ export async function podInstall(): Promise<void> {
   const { projectName } = state.answers;
 
   async function action(): Promise<void> {
-    await asyncExec(`cd ${projectName}/ios && pod install`);
+    await asyncExec('pod install', {
+      cwd: `${projectName}/ios`,
+    });
   }
 
   const spinner = createSpinner(() => action(), {
@@ -45,8 +47,8 @@ export function renameProject(): void {
 
   // Let user know we renamed the project
   logger.warning(
-    `Project name insufficient, it has been renamed to '${state.answers.projectName}'.\n`,
-    'Read more: https://github.com/facebook/react-native/issues/213.\n',
+    'Project name contains invalid characters (only lowercase allowed), so it has been renamed to ',
+    `${state.answers.projectName}.\n Read more: https://github.com/facebook/react-native/issues/213.\n`,
   );
 }
 
