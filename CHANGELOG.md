b532c184b3efd5161497fc76a89b164f9b8ecc6f
Sander Vispoel <sandervispoel@hotmail.com>
2022-05-02 13:30:16 +0200

    Delete the Next options from base eslint config

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# create-react-prime
+
+## 4.0.4
+
+### Patch Changes
+
+- Delete the CHANGELOG.md file from boilerplates during install
+- Delete the Next options from the base eslint config during install
--- src/modules/installers/shared/actions/copyBoilerplate.ts
+++ src/modules/installers/shared/actions/copyBoilerplate.ts
@@ -1,7 +1,16 @@
 import fs from 'fs/promises';
+import type { JsonObject } from 'type-fest';
 import { state, asyncExec } from '@crp';
 
 import { DOWNLOADED_MONOREPO_FOLDER_NAME } from 'src/modules/constants';
+
+type EslintJsonObject = JsonObject & {
+  settings?: {
+    next?: {
+      rootDir?: string[];
+    };
+  };
+};
 
 export async function copyBoilerplate(): Promise<void> {
   const { boilerplate, projectName } = state.answers;
@@ -18,6 +27,20 @@ export async function copyBoilerplate(): Promise<void> {
 
   const raw = await fs.readFile(`${projectName}/.eslintrc`, 'utf8');
   const next = raw.replace('../../.eslintrc', '.eslintrc.base.json');
+
+  const eslintJson: EslintJsonObject = JSON.parse(
+    await fs.readFile(`${projectName}/.eslintrc.base.json`, 'utf8'),
+  );
+
+  // Delete the next settings if they exist. These are only required for the monorepo
+  if (eslintJson?.settings?.next != null) {
+    delete eslintJson.settings.next;
+
+    await fs.writeFile(
+      `${projectName}/.eslintrc.base.json`,
+      JSON.stringify(eslintJson, null, 2),
+    );
+  }
 
   await fs.writeFile(`${projectName}/.eslintrc`, next);
 }
